a766c9fa3fe059bf4ea4e3c210cd62a6b8283c62
rafiizky17-arch <rafiizky17@gmail.com>
2026-06-25 16:33:09 +0700

    Initialize main.py with welcome messages and function

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Welcome to Elite Science Tech!")
+print("Sistem berhasil diinisialisasi.")
+
+# Kamu bisa tambah kode kodingan lainnya di bawah ini
+def sapa_pengguna(nama):
+    return f"Halo {nama}, selamat datang di proyek koding pertamamu!"
+
+print(sapa_pengguna("CEO"))
